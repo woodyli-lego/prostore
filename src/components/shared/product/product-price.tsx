5ddb2ae15d1bef53667b8b85e9df836984960646
Woody Li <woody.li@lego.com>
2025-01-29 14:49:45 +0800

    product price

---
+++ src/components/shared/product/product-price.tsx
@@ -0,0 +1,18 @@
+import { cn } from "@/lib/utils";
+
+const ProductPrice = ({ value, className }: { value: number; className?: string}) => {
+  // 确认小数只有两位
+  const stringValue = value.toFixed(2);
+  // 分开整数和小数部分
+  const [intValue, floatValue] = stringValue.split('.')
+
+  return (
+    <p className={cn('text-2xl', className) }>
+      <span className="text-xs align-super">$</span>
+      {intValue}
+      <span className="text-xs align-super">.{floatValue}</span>
+    </p>
+  );
+}
+
+export default ProductPrice;
