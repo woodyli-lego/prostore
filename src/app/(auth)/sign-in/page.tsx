67dd569f58aafb7cd317529f8e87dbaf594ae916
Woody Li <woody.li@lego.com>
2025-01-30 13:51:45 +0800

    sign in form

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -3,6 +3,7 @@ import { APP_NAME } from "@/lib/constants";
 import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
+import CredentialsSignInForm from "./credentials-signin-form";
 
 export const metadata: Metadata = {
   title: "Sign In",
@@ -33,7 +34,7 @@ const SignInPage = () => {
           </CardDescription>
         </CardHeader>
         <CardContent className="space-y-4">
-          {/* form here */}
+          <CredentialsSignInForm />
         </CardContent>
       </Card>
     </div>
