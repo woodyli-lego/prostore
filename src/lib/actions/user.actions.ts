4f336c9b07e6d8739b18d8e6c74c10d18921dbd8
Woody Li <woody.li@lego.com>
2025-01-30 23:22:11 +0800

    sign up action

--- src/lib/actions/user.actions.ts
+++ src/lib/actions/user.actions.ts
@@ -1,30 +1,32 @@
 "use server";
 
-import { signInFormSchema } from "../validators";
+import { signInFormSchema, signUpFormSchema } from "../validators";
 import { signIn, signOut } from "@/auth";
+import { prisma } from "@/db/prisma";
+import { hashSync } from "bcrypt-ts-edge";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
 
 // 使用用户名密码登录
 export async function signInwithCredentials(prevState: unknown, formData: FormData) {
   try {
     const user = signInFormSchema.parse({
-      email: formData.get('email'),
-      password: formData.get('password')
-    })
+      email: formData.get("email"),
+      password: formData.get("password"),
+    });
 
-    await signIn('credentials', user);
+    await signIn("credentials", user);
     return {
       success: true,
-      message: 'Signed in successfully'
-    }
+      message: "Signed in successfully",
+    };
   } catch (error) {
     if (isRedirectError(error)) {
       throw error;
     }
     return {
       success: false,
-      message: 'Invalid email or password'
-    }
+      message: "Invalid email or password",
+    };
   }
 }
 
@@ -32,4 +34,44 @@ export async function signInwithCredentials(prevState: unknown, formData: FormDa
 export async function signOutUser() {
   // kill session, cookie, etc.
   await signOut();
+}
+
+// 用户注册
+export async function signUpUser(prevState: unknown, formData: FormData) {
+  try {
+    const user = signUpFormSchema.parse({
+      name: formData.get("name"),
+      email: formData.get("email"),
+      password: formData.get("password"),
+      confirmPassword: formData.get("confirmPassword"),
+    });
+
+    const plainPassword = user.password;
+    user.password = hashSync(user.password, 10);
+    await prisma.user.create({
+      data: {
+        name: user.name,
+        email: user.email,
+        password: user.password,
+      },
+    });
+
+    await signIn("credentials", {
+      email: user.email,
+      password: plainPassword,
+    });
+
+    return {
+      success: true,
+      message: 'User registered successfully'
+    }
+  } catch (error) {
+    if (isRedirectError(error)) {
+      throw error;
+    }
+    return {
+      success: false,
+      message: "User was not registered",
+    };
+  }
 }
--- src/lib/validators.ts
+++ src/lib/validators.ts
@@ -25,6 +25,19 @@ export const insertProductSchema = z.object({
 
 // Schema for signing users in
 export const signInFormSchema = z.object({
-  email: z.string().email('Invalid email address'),
-  password: z.string().min(6, 'Password must be at least 6 characters')
-})
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(6, "Password must be at least 6 characters"),
+});
+
+// Schema for signing up a user
+export const signUpFormSchema = z
+  .object({
+    name: z.string().min(3, "Name must be at least 3 characters"),
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(6, "Password must be at least 6 characters"),
+    confirmPassword: z.string().min(6, "Password must be at least 6 characters"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ["confirmPassword"],
+  });
