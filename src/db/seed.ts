72f8d2205f34a10d1a3f97616460dc49442d3d55
Woody Li <woody.li@lego.com>
2025-01-30 12:17:28 +0800

    user seed data

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -3,10 +3,19 @@ import sampleData from "./sample-data";
 
 async function main() {
   const prisma = new PrismaClient();
+
   await prisma.product.deleteMany();
   await prisma.product.createMany({
     data: sampleData.products,
   });
+
+  await prisma.account.deleteMany();
+  await prisma.session.deleteMany();
+  await prisma.verificationToken.deleteMany();
+  await prisma.user.deleteMany();
+  await prisma.user.createMany({
+    data: sampleData.users
+  })
 
   console.log("database seeded successfully!");
 }
