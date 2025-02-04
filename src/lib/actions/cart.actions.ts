d77a0356ab1c9e384921d714177b15ba60ffda02
Woody Li <woody.li@lego.com>
2025-02-04 23:34:06 +0800

    add to cart (in progress)

--- src/lib/actions/cart.actions.ts
+++ src/lib/actions/cart.actions.ts
@@ -1,12 +1,28 @@
 "use server";
 
 import { CartItem } from "@/types";
-import { convertToPlainObject, formatError } from "../utils";
+import { convertToPlainObject, formatError, round2 } from "../utils";
 import { cookies } from "next/headers";
 import { sessionCartIdKey } from "../constants";
 import { auth } from "@/auth";
 import { prisma } from "@/db/prisma";
-import { cartItemSchema } from "../validators";
+import { cartItemSchema, insertCartSchema } from "../validators";
+import { revalidatePath } from "next/cache";
+
+// Calculate cart prices
+const calcPrice = (items: CartItem[]) => {
+  const itemsPrice = round2(items.reduce((acc, item) => acc + Number(item.price) * item.qty, 0)),
+    shippingPrice = round2(itemsPrice > 100 ? 0 : 10),
+    taxPrice = round2(0.15 * itemsPrice),
+    totalPrice = round2(itemsPrice + taxPrice + shippingPrice);
+
+  return {
+    itemsPrice: itemsPrice.toFixed(2),
+    shippingPrice: shippingPrice.toFixed(2),
+    taxPrice: taxPrice.toFixed(2),
+    totalPrice: totalPrice.toFixed(2),
+  };
+};
 
 export async function addItemToCart(data: CartItem) {
   try {
@@ -28,14 +44,25 @@ export async function addItemToCart(data: CartItem) {
     const product = await prisma.product.findFirst({
       where: { id: item.productId },
     });
+    if (!product) {
+      throw new Error("Product not found");
+    }
 
-    // TESTING
-    console.log({
-      "sessionCartId:": sessionCartId,
-      'userId': userId,
-      'item': item,
-      'product': product,
-    });
+    if (!cart) {
+      const newCart = insertCartSchema.parse({
+        userId: userId,
+        items: [item],
+        sessionCartId: sessionCartId,
+        ...calcPrice([item]),
+      });
+
+      await prisma.cart.create({
+        data: newCart,
+      });
+
+      // Revalidate product page
+      revalidatePath(`/product/${product.slug}`);
+    }
 
     return {
       success: true,
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -32,3 +32,14 @@ export async function formatError(error: any) {
     return typeof error.message === "string" ? error.message : JSON.stringify(error.message);
   }
 }
+
+// Round number to 2 decimal places
+export function round2(value: number | string) {
+  if (typeof value === "number") {
+    return Math.round((value + Number.EPSILON) * 100) / 100;
+  } else if (typeof value === "string") {
+    return Math.round((Number(value) + Number.EPSILON) * 100) / 100;
+  } else {
+    throw new Error("Value is not a number or string");
+  }
+}
